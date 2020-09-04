6a360814f41ece2fae79471398a5c8da19a42f07
Thomas <44181547+hatysfriend@users.noreply.github.com>
2020-09-05 02:00:35 +1000

    More Web Component Business....

--- src/views/Components/anotherComponent/renderBugComponent.js
+++ src/views/Components/anotherComponent/renderBugComponent.js
@@ -3,26 +3,28 @@ fetch("/components/anotherComponent/renderBugComponent.html")
         response.text()
         .then((res) => {
             class BugCardComponent extends HTMLElement {
-                // set bug(value) {
-                //     this._bug = value;
-                // }
+                set bug(value) {
+                    this._bug = value;
+                }
 
-                // get bug() {
-                //     return this._bug;
-                // }
+                get bug() {
+                    return this._bug;
+                }
 
                 constructor() {
                     super();
-                    // this._bug;
+                    this._bug;
 
                     const parser = new DOMParser();
                     let html = parser.parseFromString(res, 'text/html');
 
                     let shadowRoot = this.attachShadow({mode: 'open'});
                     console.log(html);
-                    shadowRoot.appendChild(html.getElementById('bugCardContent').content.cloneNode(true));
+                    shadowRoot.appendChild(html.getElementById('bugCardTemplate').content.cloneNode(true));
                     
-                    // this.getElementsByClassName('li')[0].setAttribute('bugid', this.bug);
+                    let classes = html.getElementsByClassName('list-group-item');
+                    console.log(classes);
+                    classes[0].setAttribute('bugid', this.bug);
                 }
                 
             }
